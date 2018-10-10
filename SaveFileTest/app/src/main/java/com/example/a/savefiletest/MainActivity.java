20e5622138de2cbeaf07a38156adfe37674c5ac1
YF <justhxc@gmail.com>
2018-10-10 10:35:34 +0800

    保存文件读取文件

--- SaveFileTest/app/src/main/java/com/example/a/savefiletest/MainActivity.java
+++ SaveFileTest/app/src/main/java/com/example/a/savefiletest/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.a.savefiletest;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.edit);
+        String inputText =load();
+        if (!TextUtils.isEmpty(inputText)){
+            editText.setText(inputText);
+            editText.setSelection(inputText.length());
+            Toast.makeText(this,"Restoring succeed",Toast.LENGTH_SHORT).show();;
+        }
+    }
+    /**从文件读取保存的字符*/
+    private String load() {
+        FileInputStream inputStream = null;
+        BufferedReader reader = null;
+        StringBuffer content = new StringBuffer();
+        try {
+            inputStream = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 
     @Override
