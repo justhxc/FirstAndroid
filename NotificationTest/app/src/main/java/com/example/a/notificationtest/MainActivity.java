643debc74dbdbc6ab466c327be729b1522537c78
YF <justhxc@gmail.com>
2018-10-18 18:51:22 +0800

    发送通知测试

--- NotificationTest/app/src/main/java/com/example/a/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/a/notificationtest/MainActivity.java
@@ -6,11 +6,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -36,17 +40,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 NotificationChannel channel = new NotificationChannel("default","default"
-                        ,NotificationManager.IMPORTANCE_DEFAULT);
+                        ,NotificationManager.IMPORTANCE_HIGH);
+                channel.setShowBadge(true);
+                channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
                 manager.createNotificationChannel(channel);//安卓8.0后需要创建通知管道,用于通知分类等
                 Notification notification = new NotificationCompat.Builder(this,"default")
+//                        .setChannelId("default")
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-//
+                        //角标消息
+                        .setNumber(2)
+                        //时间
                         .setWhen(System.currentTimeMillis())
+                        //点击后启动的活动
                         .setContentIntent(pendingIntent)
+                        //点击后自动消失
                         .setAutoCancel(true)
+                        //声音
+                        .setSound(Uri.fromFile(new File("/system/media/audio/ringtones/Luna.ogg")))
+                        //震动,需要在xml中声明权限
+                        .setVibrate(new long[]{0,1000,1000,1000})
+                        //LED灯
+                        .setLights(Color.GREEN,1000,1000)
+                      //  .setDefaults(NotificationCompat.DEFAULT_ALL)//默认效果
+                        //设置样式
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText("hashdhasdhajkdhkasjdhjask,hdasjkdhkjahduiahsiujkljfaioshdfidsahdsahjkhjskadhjkhasjkdhsajkhdwqhkajjdhashdisahdadskasjdhoiwhokdajskhdsajkdhk "))
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.big_image)))
+                        //弹出通知,实测 不能弹出,需要在通知中设置为紧急通知才能弹出
+//                        .setPriority(NotificationCompat.PRIORITY_MAX)
                         .build();
                 manager.notify(1,notification);
                 break;
