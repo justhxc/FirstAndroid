f257d6ba24d658b6fd56e99642da349464aff354
YF <justhxc@gmail.com>
2018-10-19 11:08:03 +0800

    http请求测试

---
+++ NetWorkTest/app/src/main/java/com/example/a/networktest/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.a.networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendRequest = findViewById(R.id.send_request);
+        responseText = findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request){
+            sendRequestWithHttpURLConnection();
+
+        }
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        //开启新线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    //对获取的流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuffer response  = new StringBuffer();
+                    String line;
+                    while ((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (reader != null){
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response ){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //这里进行UI操作,将结果显示到界面
+                responseText.setText(response);
+            }
+        });
+    }
+}
